5c5f338647ca3030fe2ffb57da3e4877ec34d5d8
Jorge Garit <jorgegarit@gmail.com>
2022-08-28 19:04:12 -0400

    added the project title and project description input sections

--- index.js
+++ index.js
@@ -4,7 +4,36 @@ const inquirer = require('inquirer');
 const generateMardown = require('./Develop/utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [// Project Title
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+    validate: inputTitle => {
+        if (inputTitle) {
+            return true;
+        } else {
+            console.log("Please enter a title for your project.")
+            return false;
+        }
+    }
+
+}, 
+
+// Project Description
+{
+    type: 'input',
+    name: 'description',
+    message: "Please add a short description of your project.",
+    validate: inputDescription => {
+        if (inputDescription) {
+            return true;
+        } else {
+            console.log("Please add a description of your project.")
+            return false;
+        }
+    }
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
