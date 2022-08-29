ef29fc52569409927ac53c5a903a3895454ecd9e
Jorge Garit <jorgegarit@gmail.com>
2022-08-28 20:38:20 -0400

    Test 3: I think I have it this time

--- dist/README.md
+++ dist/README.md
@@ -1,25 +1,23 @@
-# Hello
-   ## Hello
-    # hello
-    ## Table of Contents
-    - [Description](#description)
-    - [Installation](#install)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Languages](#languages)
-    - [Contact](#contact-section)
+# Test
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#install)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Languages](#languages)
+  - [Contact](#contact-section)
 
-    ## Description
-    hi
-    ## Installation
-    you
-    ## Usage
-    yes
-    ## License
-    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-    ## Languages
-    JavaScript,HTML
-    ## Contact
-    - Github: [username](https://github.com/username)
-    - Email: [email@email.com](mailto:user@email.com)
-  
+  ## Description
+  Test
+  ## Installation
+  Test
+  ## Usage
+  Test
+  ## License
+  [![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  ## Languages
+  JavaScript,HTML,CSS,ES6
+  ## Contact
+  - Github: [Test](https://github.com/Test)
+  - Email: [Test](mailto:user@email.com)
+
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,30 +1,30 @@
 // This J.S will contain the skeleton 
 
 const generateMarkdown = data => {
-  return `
-    # ${data.title}
-    ## Table of Contents
-    - [Description](#description)
-    - [Installation](#install)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Languages](#languages)
-    - [Contact](#contact-section)
+  return `# ${data.title}
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#install)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Languages](#languages)
+  - [Contact](#contact-section)
 
-    ## Description
-    ${data.description}
-    ## Installation
-    ${data.install}
-    ## Usage
-    ${data.usage}
-    ## License
-    ${data.license}
-    ## Languages
-    ${data.languages}
-    ## Contact
-    - Github: [${data.userName}](https://github.com/${data.userName})
-    - Email: [${data.email}](mailto:user@email.com)
-  `;
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usage}
+  ## License
+  ${data.license}
+  ## Languages
+  ${data.languages}
+  ## Contact
+  - Github: [${data.userName}](https://github.com/${data.userName})
+  - Email: [${data.email}](mailto:user@email.com)
+
+`;
 };
 
 module.exports = generateMarkdown;
